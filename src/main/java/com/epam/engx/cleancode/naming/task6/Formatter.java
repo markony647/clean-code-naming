5278349e3f97e4cec62b5c297555d1673ea0551f
Vitalii Marchenko <external.Vitalii.Marchenko@de.bosch.com>
2021-10-06 13:19:47 +0300

    Finish refactoring of task 6

--- src/main/java/com/epam/engx/cleancode/naming/task6/Formatter.java
+++ src/main/java/com/epam/engx/cleancode/naming/task6/Formatter.java
@@ -5,21 +5,24 @@ public class Formatter {
     private static final String PLUS = "+";
     private static final String PIPE = "|";
     private static final String MINUS = "-";
-    private static final String UNDERSCORE = " _ ";
+    private static final String UNDERSCORE = "_";
+    private static final String WHITESPACE = " ";
+    private static final String NEW_LINE = "\n";
+    private static final String UNDERSCORE_SURROUNDED_WITH_SPACES = WHITESPACE + UNDERSCORE + WHITESPACE;
 
 
     public String formatKeyValue(String key, String value) {
-        String content = key + UNDERSCORE + value;
-        String minuses = repeat(MINUS, content.length());
-        return PLUS +  minuses + PLUS + "\n"
-                + PIPE + content + PIPE + "\n"
-                + PLUS + minuses + PLUS + "\n";
+        String content = key + UNDERSCORE_SURROUNDED_WITH_SPACES + value;
+        String minuses = repeatMinus(content.length());
+        return PLUS + minuses + PLUS + NEW_LINE
+                + PIPE + content + PIPE + NEW_LINE
+                + PLUS + minuses + PLUS + NEW_LINE;
     }
 
-    private String repeat(String symbol, int times) {
+    private String repeatMinus(int times) {
         String result = "";
         for (int i = 0; i < times; i++)
-            result += symbol;
+            result += MINUS;
         return result;
     }
 }
